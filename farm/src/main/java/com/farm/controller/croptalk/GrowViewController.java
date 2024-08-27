b1680bb051557f611e3c4e24871366830121e10c
subin3578 <ghkdtnqls95@gmail.com>
2024-08-27 16:43:39 +0900

    login and board (#39)

---
+++ farm/src/main/java/com/farm/controller/croptalk/GrowViewController.java
@@ -0,0 +1,38 @@
+package com.farm.controller.croptalk;
+
+import java.io.IOException;
+
+import com.farm.dto.ArticleDto;
+import com.farm.service.ArticleService;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/croptalk/growview.do")
+public class GrowViewController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	 private ArticleService service = ArticleService.INSTANCE;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String no = req.getParameter("no");
+
+		// 데이터 조회
+		ArticleDto articleDto = service.selectArticle(no);
+		
+		// 공유 참조
+		req.setAttribute("articleDto", articleDto);
+		
+		// 포워드 (화면 출력)
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/croptalk/growView.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+	
+}
+
