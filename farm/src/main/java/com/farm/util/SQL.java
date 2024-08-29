a89d32d52f389c2b72b2b2183805c1b430c0b1b7
subin3578 <ghkdtnqls95@gmail.com>
2024-08-29 17:38:58 +0900

    SQL.java (#52)

--- farm/src/main/java/com/farm/util/SQL.java
+++ farm/src/main/java/com/farm/util/SQL.java
@@ -1,31 +1,30 @@
 package com.farm.util;
 
 public class SQL{
-	
-	public static final String INSERT_PRODUCT = "INSERT INTO product "
-	        + "(proname, category, price, points, discount, delivery_cost, stock, pro_img_list, pro_img_inf, pro_img_desc, etc) "
-	        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	
-	
-	
-	
-	
-	public static final String INSERT_FILE = "insert into file set"
-											+ "`oName`=?,"
-											+ "`sName`=?,"
-											+ "`rdate`=NOW()";
-	
-	// user
+
+	// USER(김보경) //////////////////////////////////////////////////////////////////////
 	public static final String SELECT_TERMS = "select * from terms";
-	
+	public static final String SELECT_FINDID = "select * from `user` where `name`=? and `hp`=?";
 	public static final String SELECT_COUNT_USER = "SELECT COUNT(*) FROM `user` ";
 	public static final String WHERE_UID  = "WHERE `uid`=?";
 	public static final String WHERE_NICK = "WHERE `nick`=?";
 	public static final String WHERE_EMAIL = "WHERE `email`=?";
 	public static final String WHERE_HP = "WHERE `hp`=?";
+	public static final String UPDATE_USER = "update `user` set "
+												+ "`pass`=?,"
+												+ "`name`=?,"
+												+ "`nick`=?,"
+												+ "`email`=?,"
+												+ "`hp`=?,"
+												+ "`zip`=?,"
+												+ "`addr1`=?,"
+												+ "`addr2`=? "
+												+ "where `uid`=?";
+		
+	public static final String SELECT_USER2 = "select * from `user` where `uid`=?";
 	public static final String SELECT_USERS = "SELECT * FROM `user`";
 	public static final String DELETE_USER = "delete from `user` where `uid`=?";
-	public static final String SELECT_USER = "select * from `user` where `uid`=? and `pass`=?";
+	public static final String SELECT_USER = "select * from `user` where `uid`=? and `pass`=SHA2(?,256)";
 	public static final String INSERT_USER = "insert into user set "
 											+ "`uid`=?,"
 											+ "`pass`=SHA2(?, 256),"
@@ -38,36 +37,71 @@ public class SQL{
 											+ "`addr2`=?,"
 											+ "`regip`=?,"
 											+ "`regDate`=NOW()";
+	/////////////////////////////////////////////////////////////////////////////////////
+	
+	// ARTICLE (황수빈) //////////////////////////////////////////////////////////////////
+	public static final String INSERT_ARTICLE = "insert into `article` set "
+											+ "`title`=?,"
+											+ "`content`=?,"
+											+ "`type`=?, "
+											+ "`cate`=?, "
+											+ "`writer`=?,"
+											+ "`regip`=?,"
+											+ "`rdate`=NOW()";
+	public static final String SELECT_ARTICLE= "SELECT a.*, b.nick FROM `article` AS a "
+											+ "JOIN `user` AS b ON a.writer = b.uid "
+											+ "WHERE `no`=?";
+
+	public static final String SELECT_ARTICLES= "SELECT a.*, b.nick  from `article` AS a "
+											+ "JOIN `user` AS b ON a.writer = b.uid "
+											+ "where a.type = ? " 
+											+ "ORDER BY `no` DESC "
+											+ "LIMIT ?, 10";
+	public static final String UPDATE_ARTICLE = "update article set `title`=?, `content`=? where `no`=?";
 	public static final String DELETE_ARTICLE= "delete from `article` where `no`=?";
 	public static final String SELECT_COUNT_TOTAL= "SELECT COUNT(*) FROM `article`"
 													+" where `type` = ?";
-
-	public static final String SELECT_ARTICLE= "SELECT a.*, b.nick FROM `article` AS a "
-												+ "JOIN `user` AS b ON a.writer = b.uid "
-												+ "WHERE `no`=?";
-	
-	public static final String SELECT_ARTICLES= "SELECT a.*, b.nick  from `article` AS a "
-												+ "JOIN `user` AS b ON a.writer = b.uid "
-												+ "where a.type = ? " 
-												+ "ORDER BY `no` DESC "
-												+ "LIMIT ?, 10";
-												
-	
 	public static final String SELECT_MAX_NO = "select MAX(`no`) from `article`";
-
-	public static final String INSERT_ARTICLE = "insert into `article` set "
-												+ "`title`=?,"
-												+ "`content`=?,"
-												+ "`type`=?, "
-												+ "`cate`=?, "
-												+ "`writer`=?,"
-												+ "`regip`=?,"
-												+ "`rdate`=NOW()";
+	//////////////////////////////////////////////////////////////////////////////////////////
 
 
+	// PRODUCT (원기연) ////////////////////////////////////////////////////////////////////////
+	public static final String INSERT_PRODUCT = "INSERT INTO product "
+		        + "(proname, category, price, points, discount, delivery_cost, stock, pro_img_list, pro_img_inf, pro_img_desc, etc) "
+		        + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	
+	public static final String INSERT_FILE = "insert into file set"
+											+ "`oName`=?,"
+											+ "`sName`=?,"
+											+ "`rdate`=NOW()";
+	
+	public static final String SELECT_PRODUCT = "SELECT * FROM `product` WHERE product_id = ?";
 
+	public static final String SELECT_PRODUCTS = "select * from `product`"
+											+ "order by product_id desc "
+											+ "limit ?, 5";
+	
+	public static final String SELECT_PRODUCTS2 = "select * from `product`";	
+	public static final String SELECT_MAX_PRODUCT_ID ="select MAX(`product_id`) from `product`";	
+	public static final String DELETE_PRODUCT = "DELETE FROM `product` WHERE product_id = ?";
+	public static final String SELECT_COUNT_TOTAL_PRO = "select count(*) from `product`";
+	public static final String DELETE_FILE = "DELETE FROM `file` WHERE product_id = ?";
+	/////////////////////////////////////////////////////////////////////////////////////
 
+	// CART (김연화) /////////////////////////////////////////////////////////////////////
+	public static final String SELECT_CART = "SELECT b.category , b.proname, a.quantity, a.discount, a.`point`, a.price, a.prodId "
+			+ "from `cart` AS a "
+			+ "JOIN `product` AS b ON a.prodId = b.product_id "
+			+ "where `uid` =?";
+	
+	public static final String INSERT_CART = "INSERT INTO `cart` "
+			+ "(`prodId`,`uid`,`quantity`,`price`) "
+			+ "values (?,?,?,?)";
+	public static final String DELETE_CART= "delete from `cart` where `cartNo`=?";
+	/////////////////////////////////////////////////////////////////////////////////////
 
-	public static final String UPDATE_ARTICLE = "update article set `title`=?, `content`=? where `no`=?";
+	// ORDER (김소진) //////////////////////////////////////////////////////////////////////
+	public static final String SELECT_ORDERS = "select * from `order`";
+	/////////////////////////////////////////////////////////////////////////////////////
 	
 }
